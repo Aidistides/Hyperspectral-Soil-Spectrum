45952a3d92cb201c16363e1a3c8c89d69a2b47aa
Aiden Young <208755803+Aidistides@users.noreply.github.com>
2026-04-03 09:55:24 -0400

    Create random_forest.py

---
+++ soil_hsi/hyperspectral-soil/models/random_forest.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestRegressor
+
+class RandomForestModel:
+    def __init__(self, n_estimators=100, random_state=42):
+        self.model = RandomForestRegressor(
+            n_estimators=n_estimators,
+            random_state=random_state
+        )
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
